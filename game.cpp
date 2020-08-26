cc9654b4aedbed681d74a6050cb536734e00cc94
Rima Avetisyan <avetisyan.rima.02@gmail.com>
2020-08-26 11:11:17 +0400

    added gameover logic

--- game.cpp
+++ game.cpp
@@ -86,12 +86,34 @@ void Logic() {
 		break;
 	}
 
-	if (x > width || x < 0 || y > height || y < 0)
-		gameOver = true;
+	///if (x > width || x < 0 || y > height || y < 0)
+	///	gameOver = true;
+
+	if (x >= width-1) {
+		x = 0;
+	}
+	else if (x < 0) {
+		x = width - 2;
+	}
+
+	if (y >= height) {
+		y = 0;
+	}
+	else if (y < 0) {
+		y = height - 1;
+	}
+
+
+	for (int i = 0; i < nTail; i++) {
+		if (tailX[i] == x && tailY[i] == y) {
+			gameOver == true;
+		}
+	}
 	if (x == fruitX && y == fruitY) {
 		score += 10;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
+		nTail++;
 	}
 }
 
